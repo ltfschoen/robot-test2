e07f9966cbad3a2fb47db571236ec43d474bafb8
Luke Schoen <ltfschoen@gmail.com>
2016-11-19 09:53:57 +1100

    Fixed incorrect declaration

--- src/board.js
+++ src/board.js
@@ -1,8 +1,8 @@
 var boardTemplate = require('../helpers/board_template.js');
 
 function Board(boardDimensions) {
-    boardDimensions = { width: 5, height: 5}; // Restrict board size to adhere to specification
-    this.layout = boardTemplate.getTemplateForDimensions(boardDimensions);
+    this.boardDimensions = { width: 5, height: 5}; // Restrict board size to adhere to specification
+    this.layout = boardTemplate.getTemplateForDimensions(this.boardDimensions);
 }
 
 module.exports = Board;
